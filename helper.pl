6dfd2d5a3e699ace062b3fb1094690b135b97a03
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-12-18 14:32:08 +0100

    Create helper.pl

---
+++ helper.pl
@@ -0,0 +1,13 @@
+% head/2 gets the first Item in List
+head([Hd | _], Hd).
+
+% tail/2 gets the tail of List
+tail([_ | Tl], Tl).
+
+% find/3 puts the index of Item into Result if it is in List
+find([Item | _], Item, Result, Result) :- !.
+find([_ | Tl], Item, I, Result) :-
+	I_ is I + 1,
+	find(Tl, Item, I_, Result).
+find(List, Item, Result) :-
+	find(List, Item, 0, Result).
