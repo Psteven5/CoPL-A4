1c8193c96dd209f03b4a45cf29ca50f45d60c293
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-12-18 14:34:49 +0100

    Update main.pl

--- main.pl
+++ main.pl
@@ -8,13 +8,13 @@ cell(Name, Result) :- find([
 	tent
 ], Name, Result).
 
+% board/4 generates a tent puzzle board of M * N with K trees
 board(0, _, Result, _, Result) :- !.
 board(I, TreeIdxs, Acc, N, Result) :-
 	I_ is I - 1,
-	( empty(TreeIdxs) ->
-		board(I_, TreeIdxs, [empty | Acc], N, Result)
-	; head(TreeIdxs) =:= N - I_ ->
-		board(I_, tail(TreeIdxs), [tree | Acc], N, Result)
+	( head(TreeIdxs, Hd), Hd =:= N - I_ ->
+ 		tail(TreeIdxs, Tl),
+		board(I_, Tl, [tree | Acc], N, Result)
 	; board(I_, TreeIdxs, [empty | Acc], N, Result)).
 board(M, N, K, Result) :-
 	Size is M * N,
