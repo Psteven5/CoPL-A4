12ec482ad78da4f3a6a585de1f12d68ab0ce8a97
Pieter Stevens <pieter.stevens@icloud.com>
2024-12-18 21:08:11 +0100

    made grid 2d

--- main.pl
+++ main.pl
@@ -8,36 +8,23 @@ print_cell(empty) :- write(' ').
 print_cell(tree)  :- write('*').
 print_cell(tent)  :- write('^').
 
-% board/4 generates a tent puzzle board of M * N with K trees
-% board(0, _, Result, _, Result) :- !.
-% board(I, TreeIdxs, Acc, N, Result) :-
-% 	I_ is I - 1,
-% 	( head(TreeIdxs, Hd), Hd =:= N - I_ ->
-%  		tail(TreeIdxs, Tl),
-% 		board(I_, Tl, [tree | Acc], N, Result)
-% 	; board(I_, TreeIdxs, [empty | Acc], N, Result)).
-% board(M, N, K, Result) :-
-% 	Size is M * N,
-% 	randset(K, Size, TreeIdxs),
-% 	board(Size, TreeIdxs, [], Size, Cells),
-% 	Result = (M, N, Cells).
-
 row(_, 0, _, _, Result2, Result1, Result1, Result2) :- !.
-row(R, C, M, N, TreeIdxs, Acc, Result1, Result2) :-
+row(R, C, M, N, TentIdxs, Acc, Result1, Result2) :-
 	C_ is C - 1,
-	( head(TreeIdxs, Hd), Hd =:= (M - R) * N + (N - C_)  ->
- 		tail(TreeIdxs, Tl),
-		row(R, C_, M, N, Tl, [tree | Acc], Result1, Result2)
-	; row(R, C_, M, N, TreeIdxs, [empty | Acc], Result1, Result2)).
+	( head(TentIdxs, Hd), Hd =:= (M - R) * N + (N - C_)  ->
+ 		tail(TentIdxs, Tl),
+		row(R, C_, M, N, Tl, [tent | Acc], Result1, Result2)
+	; row(R, C_, M, N, TentIdxs, [empty | Acc], Result1, Result2)).
 
+% board/4 generates a tent puzzle board of M * N with K trees
 board(0, _, _, _, _, Result, Result) :- !.
-board(R, _, M, N, TreeIdxs, Acc, Result) :-
-	row(R, N, M, N, TreeIdxs, [], Row, TreeIdxs_),
+board(R, _, M, N, TentIdxs, Acc, Result) :-
+	row(R, N, M, N, TentIdxs, [], Row, TentIdxs_),
 	R_ is R - 1,
-	board(R_, _, M, N, TreeIdxs_, [Row | Acc], Result).
+	board(R_, _, M, N, TentIdxs_, [Row | Acc], Result).
 board(M, N, K, Result) :-
-	randset(K, M * N, TreeIdxs),
-	board(M, N, M, N, TreeIdxs, [], Cells),
+	randset(K, M * N, TentIdxs),
+	board(M, N, M, N, TentIdxs, [], Cells),
 	Result = (M, N, Cells).
 
 print_row([]) :- !.
