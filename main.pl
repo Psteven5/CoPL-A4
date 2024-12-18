42616e3a5e8f8fca27bf751d708f11a43a63ef5f
Pieter Stevens <pieter.stevens@icloud.com>
2024-12-18 19:16:57 +0100

    added main

--- main.pl
+++ main.pl
@@ -4,6 +4,14 @@
 % a cell can be empty, a tree, or a tent
 cells([empty, tree, tent]).
 
+print_cell(Cell) :-
+	( empty == Cell ->
+		write(' ')
+	; tree == Cell ->
+		write('*')
+	; tent == Cell ->
+		write('%')).
+
 % board/4 generates a tent puzzle board of M * N with K trees
 board(0, _, Result, _, Result) :- !.
 board(I, TreeIdxs, Acc, N, Result) :-
@@ -15,4 +23,21 @@ board(I, TreeIdxs, Acc, N, Result) :-
 board(M, N, K, Result) :-
 	Size is M * N,
 	randset(K, Size, TreeIdxs),
-	board(Size, TreeIdxs, [], Size, Result).
+	board(Size, TreeIdxs, [], Size, Cells),
+	Result = (M, N, K, Cells).
+
+print_row(0, Result, Result) :- !.
+print_row(I, [Hd | Tl], Result) :-
+	print_cell(Hd), write(' '),
+	I_ is I - 1,
+	print_row(I_, Tl, Result).
+
+print_board((0, _, _, _)) :- !.
+print_board((I, N, K, Cells)) :-
+	print_row(N, Cells, Tl), write('\n'),
+	I_ is I - 1,
+	print_board((I_, N, K, Tl)).
+
+main :-
+	board(3, 3, 3, Board),
+	print_board(Board).
